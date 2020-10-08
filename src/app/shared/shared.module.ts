c2c1d8aa0a67d5322983e9119a8c0f16f8b7040c
SauravPahuja1212 <sauravpahuja1995@gmail.com>
2020-10-08 16:34:21 +0530

    Created Header and Footer and Partial Home Page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
-
-
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
